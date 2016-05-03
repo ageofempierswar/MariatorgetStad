c76fe7447f714a93ed7e5e9fd389d36937ddcfa4
linusekdahl <linusekdahl@gmail.com>
2016-05-03 13:18:36 +0200

    Contact

---
+++ MariatorgetStad/MariatorgetStadDAL/Models/Contact.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MariatorgetStadDAL.Models
+{
+    public class Contact
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Email får inte lov att vara tom")]
+        [EmailAddress(ErrorMessage = "Fel Adress, prova igen (example@gmail.com)")]
+        [DisplayName("Email")]
+        public string EmailAdress { get; set; }
+
+        [Required(ErrorMessage = "Ämne får inte lov att vara tom")]
+        [DisplayName("Ämne")]
+        public string Subject { get; set; }
+
+        [MaxLength(1000)]
+        [DataType(DataType.MultilineText)]
+        public string Message { get; set; }
+    }
+}
